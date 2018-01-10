0a87739406cc994af6c4bc278a3874aa514e02d1
anfei1972 <airflight.anfei@gmail.com>
2018-01-10 22:18:39 +1100

    EMA new file created

---
+++ EMA.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+@author: 邢不行
+
+本系列帖子“量化小讲堂”，通过实际案例教初学者使用python、pandas进行金融数据处理，希望能对大家有帮助。
+
+必读文章《10年400倍策略分享-附视频逐行讲解代码》：http://bbs.pinggu.org/thread-5558776-1-1.html
+
+所有系列文章汇总请见：http://bbs.pinggu.org/thread-3950124-1-1.html
+
+想要快速、系统的学习量化知识，可以参与我与论坛合作开设的《python量化投资入门》视频课程：http://www.peixun.net/view/1028.html，我会亲自授课，随问随答。
+参与课程还可以免费加入我的小密圈，我每天会在圈中分享量化的所见所思，圈子介绍：http://t.xiaomiquan.com/BEiqzVB
+
+微信：xbx_laoshi，量化交流Q群(快满)：438143420，有问题欢迎交流。
+
+文中用到的A股数据可在www.yucezhe.com下载，这里可以下载到所有股票、从上市日起的交易数据、财务数据、分钟数据、分笔数据、逐笔数据等。
+"""
+
+import pandas as pd
+
+# ========== 从原始csv文件中导入股票数据，以浦发银行sh600000为例
+
+# 导入数据 - 注意：这里请填写数据文件在您电脑中的路径
+stock_data = pd.read_csv('stock data/sh600000.csv', parse_dates=[1])
+
+# 将数据按照交易日期从远到近排序
+stock_data.sort('date', inplace=True)
+
+# ========== 计算移动平均线
+
+# 分别计算5日、20日、60日的移动平均线
+ma_list = [5, 20, 60]
+
+# 计算简单算术移动平均线MA - 注意：stock_data['close']为股票每天的收盘价
+for ma in ma_list:
+    stock_data['MA_' + str(ma)] = pd.rolling_mean(stock_data['close'], ma)
+
+# 计算指数平滑移动平均线EMA
+for ma in ma_list:
+    stock_data['EMA_' + str(ma)] = pd.ewma(stock_data['close'], span=ma)
+
+# 将数据按照交易日期从近到远排序
+stock_data.sort('date', ascending=False, inplace=True)
+
+# ========== 将算好的数据输出到csv文件 - 注意：这里请填写输出文件在您电脑中的路径
+stock_data.to_csv('sh600000_ma_ema.csv', index=False)
